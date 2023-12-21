f10e431888eaa518af670ff66fc929b406e81d02
adrian <adrianafnandika@gmail.com>
2023-12-21 12:24:04 +0700

    For demo

--- go.mod
+++ go.mod
@@ -2,14 +2,19 @@ module go-ff-v2
 
 go 1.21.0
 
-require github.com/thomaspoignant/go-feature-flag v1.18.2
+require (
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+	github.com/thomaspoignant/go-feature-flag v1.18.2
+	gopkg.in/yaml.v3 v3.0.1
+)
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/nikunjy/rules v1.5.0 // indirect
 	golang.org/x/exp v0.0.0-20230807204917-050eac23e9de // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,37 +1,137 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"time"
 
 	ffclient "github.com/thomaspoignant/go-feature-flag"
 	"github.com/thomaspoignant/go-feature-flag/ffcontext"
 	"github.com/thomaspoignant/go-feature-flag/retriever/fileretriever"
+	"gopkg.in/yaml.v3"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
-
-func main() {
-	initFlag()
-
-}
 
 func initFlag() {
 	ffclient.Init(ffclient.Config{
-		PollingInterval: 1 * time.Second,
+		PollingInterval: 10 * time.Second,
 		Retriever: &fileretriever.Retriever{
 			Path: "flag.yaml",
 		},
 	})
 
-	user := ffcontext.NewEvaluationContext("1")
-	allFlagsState := ffclient.AllFlagsState(user)
-	fmt.Println(allFlagsState.GetFlags()["aa"].Value)
+}
 
-	user1 := ffcontext.
-		NewEvaluationContextBuilder("4").
-		AddCustom("cohort", "xy").
+type ApiResponse struct {
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	userID := r.URL.Query().Get("user_id")
+	cohort := r.URL.Query().Get("cohort")
+
+	user := ffcontext.
+		NewEvaluationContextBuilder(userID).
+		AddCustom("cohort", cohort).
 		Build()
-	allFlagsState = ffclient.AllFlagsState(user1)
-	fmt.Println(allFlagsState.GetFlags()["aa"].Value)
+	allFlagsState := ffclient.AllFlagsState(user)
 
-	defer ffclient.Close()
+	apiResponse := ApiResponse{
+		Msg:  "success",
+		Data: allFlagsState,
+	}
+
+	jsonData, err := json.Marshal(apiResponse)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
+
+func writeToFile(fileName, content string) error {
+	err := os.WriteFile(fileName, []byte(content), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	// Read the JSON data from the request body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+
+	// Parse the JSON data into a map
+	var jsonData map[string]interface{}
+	err = json.Unmarshal(body, &jsonData)
+	if err != nil {
+		http.Error(w, "Error parsing JSON data", http.StatusBadRequest)
+		return
+	}
+
+	// Convert JSON to YAML
+	yamlData, err := yaml.Marshal(jsonData)
+	if err != nil {
+		http.Error(w, "Error converting JSON to YAML", http.StatusInternalServerError)
+		return
+	}
+
+	// Write YAML content to the file, overwriting existing content
+	fileName := "flag.yaml"
+	err = writeToFile(fileName, string(yamlData)+"\n")
+	if err != nil {
+		http.Error(w, "Error writing to file", http.StatusInternalServerError)
+		return
+	}
+
+	// Create the API response
+	apiResponse := ApiResponse{
+		Msg:  "success",
+		Data: jsonData,
+	}
+
+	// Convert the API response to JSON
+	jsonResponse, err := json.Marshal(apiResponse)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the content type and write the JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(jsonResponse)
+}
+
+func main() {
+	initFlag()
+
+	router := mux.NewRouter()
+
+	corsOptions := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+		handlers.AllowCredentials(),
+	)
+
+	router.HandleFunc("/api/get", getHandler).Methods("GET")
+	router.HandleFunc("/api/writeyaml", postHandler).Methods("POST")
+
+	port := 8080
+	fmt.Printf("nexus Server started on port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), corsOptions(router))
+
 }
